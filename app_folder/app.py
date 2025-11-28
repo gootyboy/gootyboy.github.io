bc93fd0456ccea0c929e5b8a2c9b18ca197130c7
Gooty <gootyboy@icloud.com>
2025-11-28 11:58:59 -0500

    Refactor app.py for improved readability and add requirements.txt

--- app_folder/app.py
+++ app_folder/app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request, redirect, session
-from flask_session import Session
 from cs50 import SQL
-import time
-import pyttsx3
+from flask_session import Session
+from flask import Flask, render_template, request, redirect, session
 
 db = SQL("sqlite:///spanish_dict.db")
 
@@ -12,14 +10,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-user_details = {
-    "username": "q",
-    "password": "w"
-}
+user_details = {"username": "q", "password": "w"}
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
 
 @app.route("/loggedin", methods=["GET", "POST"])
 def index_login():
@@ -27,13 +24,16 @@ def index_login():
         return render_template("index_login.html")
     return redirect("/login")
 
+
 @app.route("/login/incorrect", methods=["GET", "POST"])
 def incorrect():
     return render_template("login_fail.html")
 
+
 @app.route("/loggedin/add", methods=["POST"])
 def add():
     return render_template("add.html")
+
 
 @app.route("/addword", methods=["POST"])
 def add_word():
@@ -42,14 +42,21 @@ def add_word():
     row_type = request.form.get("type")
     print(row_type)
     if en_word != None and sp_word != None:
-        db.execute("INSERT INTO words (type, spanish, english) VALUES(?, ?, ?)", row_type, sp_word, en_word)
-    
+        db.execute(
+            "INSERT INTO words (type, spanish, english) VALUES(?, ?, ?)",
+            row_type,
+            sp_word,
+            en_word,
+        )
+
     return redirect("/loggedin/see")
+
 
 @app.route("/loggedin/see", methods=["POST", "GET"])
 def see_login():
     words = db.execute("SELECT * FROM words")
     return render_template("see_login.html", words=words)
+
 
 @app.route("/remove", methods=["POST"])
 def remove():
@@ -58,17 +65,23 @@ def remove():
 
     return redirect("/loggedin/see")
 
+
 @app.route("/see", methods=["POST", "GET"])
 def see():
     words = db.execute("SELECT * FROM words")
     return render_template("see.html", words=words)
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        session["user details"] = {"username": request.form.get("username"), "password": request.form.get("password")}
+        session["user details"] = {
+            "username": request.form.get("username"),
+            "password": request.form.get("password"),
+        }
         return redirect("/loggedin")
     return render_template("login.html")
+
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
